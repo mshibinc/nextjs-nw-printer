0d5b4eec03f553daf84021b1e81277d08316c8cd
mshibinc <m.shibin.c@gmail.com>
2022-08-04 19:24:44 +0530

    first commit 2

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from "react";
 import axios from "axios";
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-
- 
-
-  const printDoc=()=>{
-    axios
-      .post("./api/hello",{data:"lorem ipsum dhehfgrvf"})
-      .then((res) => {
-        console.log(res);
-      });
-  }
+  const [printV, setPrintV] = useState("");
+  const printDoc = () => {
+    axios.post("./api/hello", { data: printV }).then((res) => {
+      console.log(res);
+    });
+  };
   return (
     <div className={styles.container}>
+      <input type="text" onChange={(e) => setPrintV(e.target.value)} />
+      <p>{printV}</p>
       <button onClick={printDoc}>--------------printer--------------</button>
     </div>
-  )
+  );
 }
