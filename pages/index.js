a8de447ef2370e9e9a7c610fe684cb5a2dd614a0
mshibinc <38204529+mshibinc@users.noreply.github.com>
2022-08-05 11:19:41 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,32 +5,32 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   const [printV, setPrintV] = useState("");
   const printDoc = () => {
-    axios
-      .post(
-        "./api/hello",
-        { data: printV },
-        { headers: { "Content-Type": "application/json" } }
-      )
-      .then((res) => {
-        console.log(res);
+//     axios
+//       .post(
+//         "./api/hello",
+//         { data: printV },
+//         { headers: { "Content-Type": "application/json" } }
+//       )
+//       .then((res) => {
+//         console.log(res);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+    fetch("./api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({data: printV}),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.error("Error:", error);
       });
-    // fetch("./api/hello", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({data: printV}),
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log("Success:", data);
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error:", error);
-    //   });
   };
   return (
     <div className={styles.container}>
